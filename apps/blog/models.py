342adeaca8462689050adb32d68303b4925e9a08
alomia <alejandroalomia730@gmail.com>
2023-04-21 00:42:33 -0500

    adding a many-to-one relationship between users and posts

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -10,16 +10,16 @@ class Post(models.Model):
 
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name="blog_posts")
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
-    status = models.CharField(
-        max_length=2, choices=Status.choices, default=Status.DRAFT
-    )
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
 
     class Meta:
         ordering = ["-publish"]
