c76c111187eb4fb1a10612e71279cd1915306e7e
alomia <alejandroalomia730@gmail.com>
2023-05-14 18:08:08 -0500

    using a class-based view to list posts

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+
 from .models import Post
 
 
@@ -15,3 +17,9 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day)
     return render(request, "post_detail.html", {"post": post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    template_name = "post_list.html"
