f477d966bd0f3eeec109e28a656e446a49f5b5b1
alomia <alejandroalomia730@gmail.com>
2023-05-14 22:19:27 -0500

    handling forms in views

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
+from .forms import EmailPostForm
 
 
 def post_list(request):
@@ -10,12 +11,14 @@ def post_list(request):
 
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post,
-                             status=Post.Status.PUBLISHED,
-                             slug=post,
-                             publish__year=year,
-                             publish__month=month,
-                             publish__day=day)
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
     return render(request, "post_detail.html", {"post": post})
 
 
@@ -24,3 +27,19 @@ class PostListView(ListView):
     context_object_name = "posts"
     paginate_by = 3
     template_name = "post_list.html"
+
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+
+    if request.methos == "POST":
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            # ... send email
+    else:
+        form = EmailPostForm()
+    return render(request, "post_share.html", {"post": post, "form": form})
