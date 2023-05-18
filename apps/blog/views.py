36b73ba3fca593460dc99dd56536ca8e01597789
alomia <alejandroalomia730@gmail.com>
2023-05-18 14:46:37 -0500

    sending emails in views

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,8 +1,14 @@
+from django.core.mail import send_mail
 from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
 
+from environs import Env
+
 from .models import Post
 from .forms import EmailPostForm
+
+env = Env()
+env.read_env()
 
 
 def post_list(request):
@@ -32,6 +38,7 @@ class PostListView(ListView):
 def post_share(request, post_id):
     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
 
     if request.methos == "POST":
         # Form was submitted
@@ -39,7 +46,16 @@ def post_share(request, post_id):
         if form.is_valid():
             # Form fields passed validation
             cd = form.cleaned_data
-            # ... send email
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n {cd['name']}'s comments: {cd['comments']}"
+            send_mail(subject, message, env.str("EMAIL_HOST_USER"), [cd["to"]])
+
+            sent = True
+
     else:
         form = EmailPostForm()
-    return render(request, "post_share.html", {"post": post, "form": form})
+    return render(
+        request, "post_share.html", {"post": post, "form": form, "sent": sent}
+    )
